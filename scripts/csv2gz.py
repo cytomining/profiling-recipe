79c350579d921b6b7059b56e09fa648314e7dbe7
Niranj <srinivasniranj@gmail.com>
2021-04-23 11:20:15 -0400

    make script compatible with pycytominer version

--- scripts/csv2gz.py
+++ scripts/csv2gz.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import pycytominer
+import pycytominer.cyto_utils
 import sys
 
 input_filename = sys.argv[1]
@@ -9,5 +9,5 @@ output_filename = input_filename + ".gz"
 df = pycytominer.cyto_utils.load.load_profiles(input_filename)
 
 df = pycytominer.cyto_utils.output(
-    df, output_filename, compression="gzip", float_format="%.5g"
+    df, output_filename, compression_options="gzip", float_format="%.5g"
 )
