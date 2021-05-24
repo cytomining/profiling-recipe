c1ea2f416940c7b46d618ad026cfc242f826108d
Niranj <srinivasniranj@gmail.com>
2021-05-24 14:42:00 -0400

    allow steps in the config to be optional

--- profiles/profiling_pipeline.py
+++ profiles/profiling_pipeline.py
@@ -2,7 +2,7 @@
 Perform the profiling pipeline (defined in profile.py).
 """
 
-# Copied from
+# Modified from
 # https://github.com/broadinstitute/profiling-resistance-mechanisms/blob/master/0.generate-profiles/generate-profiles.py
 
 from utils import load_pipeline, create_directories
@@ -23,38 +23,44 @@ for batch in profile_config:
     for plate in profile_config[batch]:
         create_directories(batch=batch, plate=plate, pipeline=pipeline)
 
-        if pipeline["aggregate"]["perform"]:
-            print(f"Now aggregating... plate: {plate}")
-            run_pipeline.pipeline_aggregate(batch=batch, plate=plate)
+        if "aggregate" in pipeline:
+            if pipeline["aggregate"]["perform"]:
+                print(f"Now aggregating... plate: {plate}")
+                run_pipeline.pipeline_aggregate(batch=batch, plate=plate)
 
-        if pipeline["annotate"]["perform"]:
-            print(f"Now annotating... plate: {plate}")
-            run_pipeline.pipeline_annotate(batch=batch, plate=plate)
+        if "annotate" in pipeline:
+            if pipeline["annotate"]["perform"]:
+                print(f"Now annotating... plate: {plate}")
+                run_pipeline.pipeline_annotate(batch=batch, plate=plate)
 
-        if pipeline["normalize"]["perform"]:
-            print(f"Now normalizing... plate: {plate}")
-            run_pipeline.pipeline_normalize(
-                batch=batch, plate=plate, steps=pipeline["normalize"], samples="all"
-            )
+        if "normalize" in pipeline:
+            if pipeline["normalize"]["perform"]:
+                print(f"Now normalizing... plate: {plate}")
+                run_pipeline.pipeline_normalize(
+                    batch=batch, plate=plate, steps=pipeline["normalize"], samples="all"
+                )
 
-        if pipeline["normalize_negcon"]["perform"]:
-            print(f"Now normalizing to negcon... plate: {plate}")
-            run_pipeline.pipeline_normalize(
-                batch=batch,
-                plate=plate,
-                steps=pipeline["normalize_negcon"],
-                samples="Metadata_control_type == 'negcon'",
-                suffix="negcon",
-            )
+        if "normalize_negcon" in pipeline:
+            if pipeline["normalize_negcon"]["perform"]:
+                print(f"Now normalizing to negcon... plate: {plate}")
+                run_pipeline.pipeline_normalize(
+                    batch=batch,
+                    plate=plate,
+                    steps=pipeline["normalize_negcon"],
+                    samples="Metadata_control_type == 'negcon'",
+                    suffix="negcon",
+                )
 
-if pipeline["feature_select"]["perform"]:
-    print(f"Now feature selecting... level: {pipeline['feature_select']['level']}")
-    run_pipeline.pipeline_feature_select(steps=pipeline["feature_select"])
+if "feature_select" in pipeline:
+    if pipeline["feature_select"]["perform"]:
+        print(f"Now feature selecting... level: {pipeline['feature_select']['level']}")
+        run_pipeline.pipeline_feature_select(steps=pipeline["feature_select"])
 
-if pipeline["feature_select_negcon"]["perform"]:
-    print(
-        f"Now feature selecting negcon profiles... level: {pipeline['feature_select_negcon']['level']}"
-    )
-    run_pipeline.pipeline_feature_select(
-        steps=pipeline["feature_select_negcon"], suffix="negcon"
-    )
+if "feature_select_negcon" in pipeline:
+    if pipeline["feature_select_negcon"]["perform"]:
+        print(
+            f"Now feature selecting negcon profiles... level: {pipeline['feature_select_negcon']['level']}"
+        )
+        run_pipeline.pipeline_feature_select(
+            steps=pipeline["feature_select_negcon"], suffix="negcon"
+        )
